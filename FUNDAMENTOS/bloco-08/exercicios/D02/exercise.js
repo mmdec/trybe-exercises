f31b6e05e28668d874bb3096f58fcfdc385146fa
Matheus Machado <matheusmdec@hotmail.com>
2021-11-18 18:07:23 -0300

    Execícios 3 e 4 - Completo

--- FUNDAMENTOS/bloco-08/exercicios/D02/exercise.js
+++ FUNDAMENTOS/bloco-08/exercicios/D02/exercise.js
@@ -1,67 +1,67 @@
 const books = [
     {
-      id: 1,
-      name: 'As Crônicas de Gelo e Fogo',
-      genre: 'Fantasia',
-      author: {
-        name: 'George R. R. Martin',
-        birthYear: 1948,
-      },
-      releaseYear: 1991,
+        id: 1,
+        name: 'As Crônicas de Gelo e Fogo',
+        genre: 'Fantasia',
+        author: {
+            name: 'George R. R. Martin',
+            birthYear: 1948,
+        },
+        releaseYear: 1991,
     },
     {
-      id: 2,
-      name: 'O Senhor dos Anéis',
-      genre: 'Fantasia',
-      author: {
-        name: 'J. R. R. Tolkien',
-        birthYear: 1892,
-      },
-      releaseYear: 1954,
+        id: 2,
+        name: 'O Senhor dos Anéis',
+        genre: 'Fantasia',
+        author: {
+            name: 'J. R. R. Tolkien',
+            birthYear: 1892,
+        },
+        releaseYear: 1954,
     },
     {
-      id: 3,
-      name: 'Fundação',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Isaac Asimov',
-        birthYear: 1920,
-      },
-      releaseYear: 1951,
+        id: 3,
+        name: 'Fundação',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Isaac Asimov',
+            birthYear: 1920,
+        },
+        releaseYear: 1951,
     },
     {
-      id: 4,
-      name: 'Duna',
-      genre: 'Ficção Científica',
-      author: {
-        name: 'Frank Herbert',
-        birthYear: 1920,
-      },
-      releaseYear: 1965,
+        id: 4,
+        name: 'Duna',
+        genre: 'Ficção Científica',
+        author: {
+            name: 'Frank Herbert',
+            birthYear: 1920,
+        },
+        releaseYear: 1965,
     },
     {
-      id: 5,
-      name: 'A Coisa',
-      genre: 'Terror',
-      author: {
-        name: 'Stephen King',
-        birthYear: 1947,
-      },
-      releaseYear: 1986,
+        id: 5,
+        name: 'A Coisa',
+        genre: 'Terror',
+        author: {
+            name: 'Stephen King',
+            birthYear: 1947,
+        },
+        releaseYear: 1986,
     },
     {
-      id: 6,
-      name: 'O Chamado de Cthulhu',
-      genre: 'Terror',
-      author: {
-        name: 'H. P. Lovecraft',
-        birthYear: 1890,
-      },
-      releaseYear: 1928,
+        id: 6,
+        name: 'O Chamado de Cthulhu',
+        genre: 'Terror',
+        author: {
+            name: 'H. P. Lovecraft',
+            birthYear: 1890,
+        },
+        releaseYear: 1928,
     },
-  ];
-  
-  // Adicione o código do exercício aqui:
+];
+
+// Adicione o código do exercício aqui:
 
 //1 - Encontre o nome da primeira pessoa autora do livro nascida no ano de 1947. 
 function authorBornIn1947(books) {
@@ -74,13 +74,38 @@ function smallerName(books) {
     let nameBook;
     let aux;
     books.forEach(book => {
-        if(aux === undefined){
+        if (aux === undefined) {
             aux = book.name.length;
         }
-        if(book.name.length < aux){
+        if (book.name.length < aux) {
             nameBook = book.name;
             aux = book.name.length;
         }
     });
     return nameBook;
+}
+
+//3 - Encontre o primeiro livro cujo nome possui 26 caracteres. 
+function getNamedBook(books) {
+    return books.find(book => book.name.length === 26)
+}
+
+//4 - Ordene os livros por data de lançamento em ordem decrescente. 
+function booksOrderedByReleaseYearDesc(books) {
+    return books.sort((firstbook, secondbook) => secondbook.releaseYear - firstbook.releaseYear)
+}
+
+//5 - Faça uma função que retorne true , se todas as pessoas autoras nasceram no século XX, ou false , caso contrário. 
+function everyoneWasBornOnSecXX() {
+    // escreva seu código aqui
+  }
+
+  // 6 - Faça uma função que retorne true , se algum livro foi lançado na década de 80, e false , caso contrário. 
+  function someBookWasReleaseOnThe80s() {
+    // escreva seu código aqui
+  }
+
+  // 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário. 
+  function authorUnique() {
+    // escreva seu código aqui
   }
