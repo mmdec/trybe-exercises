bfa7696a2bf3de0ca0dd3abec279d3a725a46fe0
Matheus Machado <matheusmdec@hotmail.com>
2021-11-18 18:39:27 -0300

    Requisito 7 - Completo

--- FUNDAMENTOS/bloco-08/exercicios/D02/exercise.js
+++ FUNDAMENTOS/bloco-08/exercicios/D02/exercise.js
@@ -106,6 +106,9 @@ function someBookWasReleaseOnThe80s(books) {
 }
 
 // 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário. 
-function authorUnique() {
-    
+function authorUnique(books) {
+    return books.every((book) =>
+    !books.some((bookSome) =>
+      (bookSome.author.birthYear === book.author.birthYear)
+      && (bookSome.author.name !== book.author.name)));
 }
