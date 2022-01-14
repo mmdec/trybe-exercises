2490a95f8c4705824155790ff7203de3a5686b51
Matheus Machado <matheusmdec@hotmail.com>
2022-01-14 00:09:50 -0300

    Setup Inicial

---
+++ FUNDAMENTOS/bloco-09/callbacks/exercise.js
@@ -0,0 +1,14 @@
+const userFullName = ({ firstName, lastName }) => `Hello! My name is ${firstName} ${lastName}`;
+const userNationality = ({ firstName, nationality }) => `${firstName} is ${nationality}`;
+
+const getUser = () => {
+    const userToReturn = {
+        firstName: "Ivan",
+        lastName: "Ivanovich",
+        nationalit: "Russian"
+    };
+
+};
+
+console.log(getUser());
+console.log(getUser());
